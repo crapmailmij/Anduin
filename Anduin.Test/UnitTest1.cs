b71d2b8b7a56b23586efb5d4c5f646e9ce30225c
Lennaert Eijsvogel <Lennaert.Eijsvogel@calco.nl>
2022-09-03 19:40:23 +0200

    add test class

---
+++ Anduin.Test/UnitTest1.cs
@@ -0,0 +1,17 @@
+using MvvmCross.Tests;
+using System;
+using Xunit;
+
+namespace Anduin.Test
+{
+    public class UnitTest1 : MvxIoCSupportingTest
+    {
+        [Fact]
+        public void TestViewModel()
+        {
+            base.Setup(); // from MvxIoCSupportingTest
+
+            // your test code
+        }
+    }
+}
